d887202db8f0aeb2584aa3ab643605bcf2bee044
KAprachir <kaprachir23@gmail.com>
2026-05-02 15:27:24 +0600

    fix: remove hardcoded auth baseURL for Vercel deployment

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,8 +1,6 @@
 import { createAuthClient } from 'better-auth/react'
 
-export const authClient = createAuthClient({
-  baseURL: 'http://localhost:3000' // Make sure this matches your dev URL
-})
+export const authClient = createAuthClient();
 
 // Destructure from the instance created above
 export const { signIn, signUp, useSession, signOut } = authClient
